61164fd1db22f945baa4251b5aaf736a8a9e49f3
Taka200 <tncyberport@gmail.com>
2019-02-19 06:35:38 +0800

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by Taka on 2019/02/19.
+//  Copyright © 2019 Taka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
